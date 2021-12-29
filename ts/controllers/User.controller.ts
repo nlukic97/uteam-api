3e125062c9bb9fe49500d7f7cfe899cbfa0fc794
Nikola <nlukic97@gmail.com>
2021-12-29 21:32:35 +0100

    validating if the user submitted the object with the keys needed (username, email, password) etc

--- ts/controllers/User.controller.ts
+++ ts/controllers/User.controller.ts
@@ -2,6 +2,7 @@ import User from '../models/User'
 import { Request, Response } from 'express'
 import validator from 'validator'
 import * as bcrypt from 'bcryptjs';
+import {Op} from 'sequelize';
 
 interface ReqRes {
   (req: Request, res: Response): unknown
@@ -17,19 +18,58 @@ const getAllUsers: ReqRes = async (req, res) => {
 
 /* --- User registration --- */
 const register: ReqRes = async (req, res) => {
+  const keys = Object.keys(req.body)
+  if(!keys.includes('username') || !keys.includes('email') || !keys.includes('password')){
+    return res.status(403).json({message:'You must submit an email, username, and password.'})
+  }
+
+
   // Trimming and sanitizing inputs
   const data = {
     username: validator.trim(req.body.username + '').toLowerCase(),
     email: validator.trim(req.body.email + '').toLowerCase(),
     password: validator.trim(req.body.password + '')
-  }  
+  }
 
+  /** Checking if a user with the same email and/or username exists, and tailoring the error message */
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const userExists:any = await User.findOne({
+    where:{
+      [Op.or]:[
+        {email:data.email},
+        {username:data.username},
+      ]
+    }
+    })
+
+  if(userExists){
+    const match = [];
+    
+    if(userExists.email === data.email){
+      match.push('email')
+    }
+
+    if(userExists.username === data.username){
+      match.push('userame')
+    }
+
+    const foundMsg = (match.length === 2) ? 'username and email have' : `${match[0]} has`
+    return res.status(403).json({message:`The ${foundMsg} already been taken.`})
+  }
+  
 
   if(!validator.isEmail(data.email)){
     return res.status(403).json({ message: 'Please make sure to enter valid email credentials.'})
 
   } else if (!/^[a-z0-9._]+$/.test(data.username)){ //username can contain letters, numbers, . and _
     return res.status(403).json({ message: 'Please enter a valid username - only lowercase letters, numbers, . and _ are allowed.'})
+  
+  } else if(data.username.length < 3){
+    return res.status(403).json({ message: 'Your username must contain at least 3 characters.'})
+
+  } else if(data.password.length < 6){
+    return res.status(403).json({ message: 'Your password should be at least 6 characters long.'})
   }
 
   const salt = bcrypt.genSaltSync(10);
@@ -62,17 +102,19 @@ const register: ReqRes = async (req, res) => {
 
 /* --- User login --- */
 const login: ReqRes = async (req, res)=>{
+
+  // Making sure the 
+  const keys = Object.keys(req.body)
+  if(!keys.includes('name') || !keys.includes('password')){
+    return res.status(403).json({message:'You must submit an email / username, and password.'})
+  }
+
+  // string sanitization 
   const name: string = validator.trim(req.body.name + '').toLowerCase()
   const password: string = validator.trim(req.body.password + '')
 
+  // determining if the user submitted an email or a username (used to find the user in the db to compare the login credentials against)
   const key: string = (validator.isEmail(req.body.name)) ? 'email': 'username'
-
-/*   interface User {
-    id: number,
-    username: string,
-    email:string,
-    password:string,
-  } */
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const user: any = await User.findOne({
@@ -85,6 +127,7 @@ const login: ReqRes = async (req, res)=>{
     return res.status(404).json({message:`There is no user with this ${key}`})
   }
 
+  // comparing the submitted password with the user password in the db
   const result: boolean = bcrypt.compareSync(password, user.password)
   if(result === true){
     return res.status(200).json({message:'You are logged in'})
