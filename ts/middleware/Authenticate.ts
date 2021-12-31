ce786b5e3a6071e893e5f693b10df419f4f87c96
Nikola <nlukic97@gmail.com>
2021-12-31 01:12:46 +0100

    fixed previous bug

--- ts/middleware/Authenticate.ts
+++ ts/middleware/Authenticate.ts
@@ -3,19 +3,15 @@ import {Request, Response, NextFunction as Next } from 'express';
 import jwt from 'jsonwebtoken'
 
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface RequestExtra extends Request {
-  user: jwt.JwtPayload | undefined; //adding this removes the error at line 25. But causes a 'no overload matches this call' error in routes.ts
-}
-
-const authenticateToken = (req: RequestExtra, res: Response, next: Next) =>{
+const authenticateToken = (req: Request, res: Response, next: Next) =>{
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader?.split(' ')[1]
     if(token === null) {
       return res.sendStatus(401)
     }
   
-    jwt.verify(token as string, process.env.ACCESS_TOKEN_SECRET as string, (err,user)=>{
+    //not sure how to fix this here, I cannot set anything to 'token' on line 8
+    jwt.verify(token as string, process.env.ACCESS_TOKEN_SECRET, (err,user)=>{
       if(err) {
         return res.sendStatus(403)
       }
