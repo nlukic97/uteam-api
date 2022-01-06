4ea7a3a5516074fd371619f198293eda2fb37dbd
Nikola <nlukic97@gmail.com>
2022-01-06 15:19:09 +0100

    Updated the validation for user inputs. Also, changed getAllusers route and method to countAllUsers

--- ts/controllers/User.controller.ts
+++ ts/controllers/User.controller.ts
@@ -11,32 +11,60 @@ interface ReqRes {
 
 // methods
 /* --- Getting the number of users */
-const getAllUsers: ReqRes = async (req, res) => {
-  const users = await User.findAll()
+const countAllUsers: ReqRes = async (req, res) => {
+  const userCount = await User.count()
   res
     .status(200)
-    .json({ message: `There are currently ${users.length} users.` })
+    .json({ message:`There are currently ${userCount} users.`})
 }
 
 /* --- User registration --- */
 const register: ReqRes = async (req, res) => {
-  const keys = Object.keys(req.body)
-  if(!keys.includes('username') || !keys.includes('email') || !keys.includes('password')){
-    return res.status(403).json({message:'You must submit an email, username, and password.'})
+  const sentData:{username:string;email:string,password:string}  = req.body
+
+  // making sure all necessary data is there
+  if(!sentData.username || !sentData.email || !sentData.password){
+    return res.status(400).json({message:'Username, email, and / or password is / are missing.'})
+  }
+
+  // Checking if it is a non empty string
+  if(typeof(sentData.username) !== 'string' || sentData.username === ''
+    || typeof(sentData.email) !== 'string' || sentData.email === ''
+    || typeof(sentData.password) !== 'string' || sentData.password === ''
+  ){
+    return res.status(400).json({message:'Please make sure you submited the username, email, and password in the correct format (a non empty string).'})
   }
 
   // Trimming and sanitizing inputs
   const data = {
-    username: validator.trim(req.body.username + '').toLowerCase(),
-    email: validator.trim(req.body.email + '').toLowerCase(),
-    password: validator.trim(req.body.password + '')
+    username: validator.trim(sentData.username + '').toLowerCase(),
+    email: validator.trim(sentData.email + '').toLowerCase(),
+    password: validator.trim(sentData.password + '')
   } 
 
-  // validation
-  if(!validator.isEmail(data.email)) return res.status(403).json({ message: 'Please make sure to enter valid email credentials.'})
-  if (!/^[a-z0-9._]+$/.test(data.username)) return res.status(403).json({ message: 'Please enter a valid username - only lowercase letters, numbers, . and _ are allowed.'})
-  if(data.username.length < 3) return res.status(403).json({ message: 'Your username must contain at least 3 characters.'})
-  if(data.password.length < 6) return res.status(403).json({ message: 'Your password should be at least 6 characters long.'})
+  /** 
+   * Validation 
+   * */
+
+  // email check
+  if(!validator.isEmail(data.email)){
+    return res.status(403).json({ message: 'Please make sure to enter valid email credentials.'})
+  }
+
+  // password format check
+  if (!/^[a-z0-9#%-_*]+$/.test(data.username)){
+    return res.status(403).json({ message: 'Please enter a valid username - only lowercase letters, numbers, . and _ are allowed.'})
+  }
+
+  // username length check
+  if(data.username.length < 3){
+    return res.status(403).json({ message: 'Your username must contain at least 3 characters.'})
+  }
+
+  //password length check
+  if(data.password.length < 6){
+    return res.status(403).json({ message: 'Your password should be at least 6 characters long.'})
+  }
 
 
   /** Checking if a user with the same email and/or username exists, and tailoring the error message */
@@ -53,7 +81,7 @@ const register: ReqRes = async (req, res) => {
     const match = [];
     
     if(userExists.email === data.email) match.push('email')
-    if(userExists.username === data.username) match.push('userame')
+    if(userExists.username === data.username) match.push('username')
 
     const foundMsg = (match.length === 2) ? 'username and email have' : `${match[0]} has`
     return res.status(403).json({message:`The ${foundMsg} already been taken.`})
@@ -90,19 +118,27 @@ const register: ReqRes = async (req, res) => {
 
 /* --- User login --- */
 const login: ReqRes = async (req, res)=>{
-
-  // Making sure the 
-  const keys:Array<string|null> = Object.keys(req.body)
-  if(!keys.includes('name') || !keys.includes('password')){
-    return res.status(403).json({message:'You must submit an email / username, and password.'})
+  const sentData:{name:string;password:string} = req.body
+  
+  // making sure all necessary data is there
+  if(!sentData.name || !sentData.password) {
+    return res.status(400).json({message:'Please make sure you submitted all the necessary credentials (username or email, and password).'})
   }
 
+  // Checking if it is a non empty string
+  if(typeof(sentData.name) !== 'string' || sentData.name === ''
+  || typeof(sentData.password) !== 'string' || sentData.password === ''
+  ){
+    return res.status(400).json({message:'Please make sure you submited the username/email and password in the correct format (a non empty string).'})
+  }
+
+
   // string sanitization 
-  const name: string = validator.trim(req.body.name + '').toLowerCase()
-  const password: string = validator.trim(req.body.password + '')
+  const name: string = validator.trim(sentData.name + '').toLowerCase()
+  const password: string = validator.trim(sentData.password + '')
 
   // determining if the user submitted an email or a username (used to find the user in the db to compare the login credentials against)
-  const key: string = (validator.isEmail(req.body.name)) ? 'email': 'username'
+  const key: string = (validator.isEmail(sentData.name)) ? 'email': 'username'
 
   const user = await User.findOne({
     where: {
@@ -117,8 +153,7 @@ const login: ReqRes = async (req, res)=>{
   // comparing the submitted password with the user password in the db
   const result: boolean = bcrypt.compareSync(password, user.password)
   if(result === true){
-    
-    const accessToken: string | undefined = jwt.sign({username: user.username, id: user.id}, process.env.ACCESS_TOKEN_SECRET)
+    const accessToken: string | undefined = jwt.sign({username: user.username}, process.env.ACCESS_TOKEN_SECRET)
     return res.status(200).json({message:'You are logged in', accessToken: accessToken})
   } else {
     return res.status(401).json({message:'Incorrect password.'})
@@ -127,7 +162,7 @@ const login: ReqRes = async (req, res)=>{
 
 //   method export
 const UserController = {
-  getAllUsers,
+  countAllUsers,
   register,
   login
 }
