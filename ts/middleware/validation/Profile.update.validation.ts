0c8250d3aada8479169d038636b6a586f30ea401
nlukic97 <nlukic97@gmail.com>
2022-02-22 15:25:58 +0100

    updating validation for PUT profile

--- ts/middleware/validation/Profile.update.validation.ts
+++ ts/middleware/validation/Profile.update.validation.ts
@@ -14,61 +14,48 @@ const ProfileUpdateValidation = (req:Request,res:Response,next:Next) =>{
         company?: number,
     } = {}
     
-    /** Validation of submitted data - checking if an optional parameter has been submitted as an empty string. */
-    if(req.body.name !== undefined){        
-        if(req.body.name === ''){            
-            return res.status(400).json({message:'There appear to be empty fields. Please check your inputs and try again.'})
-        } else {
-            submitData.name = req.body.name
+    /** Validation of submitted name */
+    if(req.body.name !== undefined){
+        if(typeof req.body.name !== 'string'){
+            return res.status(400).json({message:'The name should be a string.'})
         }
+        
+        req.body.name = req.body.name.trim()
+        if(!req.body.name){
+            return res.status(400).json({message:'The name cannot be an empty string.'})
+        }
+        submitData.name = req.body.name
     }
-
+    
+    /** Validation of submitted profilePhoto */
     if(req.body.profilePhoto !== undefined){
-        if(req.body.profilePhoto === ''){
-            return res.status(400).json({message:'There appear to be empty fields. Please check your inputs and try again.'})
-        } else {
-            submitData.profilePhoto = req.body.profilePhoto
+        if(typeof req.body.profilePhoto !== 'string'){
+            return res.status(400).json({message:'The profilePhoto should be a string.'})
         }
-    }
 
-    if(req.body.company !== undefined){        
-        if(req.body.company === ''){            
-            return res.status(400).json({message:'There appear to be empty fields. Please check your inputs and try again.'})
-        } else {
-            submitData.company = +req.body.company
+        req.body.profilePhoto = req.body.profilePhoto.trim()
+        if(!req.body.profilePhoto){
+            return res.status(400).json({message:'The profilePhoto cannot be an empty string.'})
         }
+        submitData.profilePhoto = req.body.profilePhoto
+    }
+    
+    /** Validation of submitted company */
+    if(req.body.company !== undefined){
+        if(req.body.company === ''){
+            return res.status(400).json({message:'Company cannot be empty - please submit a valid company id.'})
+        }
+        if(Number.isInteger(+req.body.company) === false){
+            return res.status(400).json({message:'The company should be an integer.'})
+        }
+
+        submitData.company = req.body.company
     }
     
     
     // making sure at least one of the editable parameters is present
     if(!submitData.name && !submitData.profilePhoto && !submitData.company){
         return res.status(400).json({message:'No data submitted for update. Please submit the fields you would like to change (name, profilePhoto, and / or company).'})
-    }
-    
-    
-    //checking that present submitData is of correct type
-    if(
-       (submitData.name && typeof(submitData.name) !== 'string') 
-    || (submitData.profilePhoto && typeof(submitData.profilePhoto) !== 'string')
-    || (submitData.company && (Number.isInteger(submitData.company) === false || submitData.company === 0))
-    ) {
-        return res.status(400).json({message:'Please make sure the your submitted inputs of the correct format.'})
-    }
-    
-    // trimming the name if it was submitted, and checking that a value is still left after we trim (deals with submitted spaces as values)
-    if(submitData.name){
-        submitData.name = submitData.name.trim()
-        if(!submitData.name){
-            return res.status(400).json({message:'Please make sure the name is of the correct format.'})
-        }
-    }
-    
-    // trimming the profilePhoto if it was submitted, and checking that a value is still left after we trim (deals with submitted spaces as values)
-    if(submitData.profilePhoto){
-        submitData.profilePhoto = submitData.profilePhoto.trim()
-        if(!submitData.profilePhoto){
-            return res.status(400).json({message:'Please make sure the profilePhoto is of the correct format.'})
-        }
     }
     
     req.body = submitData
